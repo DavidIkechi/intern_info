dee1d9482c2bdfad37eba5d99a6e5eff1c1dedba
David Akwuru <daakwuru@gmail.com>
2022-06-23 15:20:49 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    // using both alert and console.
+    // to be on a safe side.
+    alert(
+        " Name: Akwuru David Chibuike Ikechi\n" +
+        " Height: 5.8ft\n" +
+        " Country: Nigeria"
+    );
+
+    console.log(
+        " Name: Akwuru David Chibuike Ikechi\n" +
+        " Height: 5.8ft\n" +
+        " Country: Nigeria"
+    );
+};
